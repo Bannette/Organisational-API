eebb28e481ba8915add4c6b25ab774f94f15dd06
Bannette <banetaloo3@gmail.com>
2019-08-09 14:44:11 +0300

    added a user interface

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,6 +1,25 @@
 package dao;
 
-public class UserDao {
-}
+import models.User;
+import models.Department;
 
+import java.util.List;
+
+public interface UserDao {
+    //create
+    void add(User user);
+
+
+    //read
+    List<User> getAll();
+    User findById(int id);
+    List<User> getAllUsersByDepartment(int departmentId);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+}
 
